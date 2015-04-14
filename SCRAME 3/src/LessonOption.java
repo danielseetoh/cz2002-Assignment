75370686a4938ed644e4f4b443e030ba0a353565
danielseetoh <danielseetoh92@gmail.com>
2015-04-14 16:11:08 +0800

    uniapp LessonOption

--- SCRAME 3/src/LessonOption.java
+++ SCRAME 3/src/LessonOption.java
@@ -1,5 +1,20 @@
 /**
  * Created by KianBoon on 14/4/2015.
  */
-public enum LessonOption { LECTURE, TUTORIAL, LAB
+public enum LessonOption {
+    LECTURE{
+        public String toString(){
+            return "Lecture";
+        }
+    },
+    TUTORIAL{
+        public String toString(){
+            return "Tutorial";
+        }
+    },
+    LAB{
+        public String toString(){
+            return "Lab";
+        }
+    }
 }
