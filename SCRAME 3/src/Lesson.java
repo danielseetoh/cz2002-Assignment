05abee28c70a7b0916a1e8f9c455b3ea0c1a3a62
kw01sg <kianboonkoh@gmail.com>
2015-04-15 15:39:23 +0800

    Lesson

--- SCRAME 3/src/Lesson.java
+++ SCRAME 3/src/Lesson.java
@@ -1,47 +1,41 @@
 
 public class Lesson {
 
+    //instance variables
     protected int ID;
     protected int capacity;
     protected int vacancies;
     protected String name;
 
+    //constructor
     public Lesson(int ID, int capacity){
-
         this.ID = ID;
         this.capacity = capacity;
-        this.vacancies = capacity;
-
+        this.vacancies = capacity;  //initialize vacancy = capacity. No registration yet
     }
 
+    //get vacancies
     public int getVacancies(){
-
         return this.vacancies;
-
     }
 
+    //get ID
     public int getID(){
-
         return this.ID;
-
     }
 
+    //get capacity
     public int getCapacity(){
-
         return this.capacity;
-
     }
 
-    public void setVacancies(){
-
-        vacancies--;
-
-    }
-
+    //get name
     public String getName(){
-
         return this.name;
-
     }
 
+    //minus vacancy by 1
+    public void setVacancies(){
+        vacancies--;
+    }
 }
