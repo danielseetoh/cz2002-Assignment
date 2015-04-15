9aa380dec98fc1d339d9ad766a415efa068ce5ea
kw01sg <kianboonkoh@gmail.com>
2015-04-15 15:31:01 +0800

    Lesson Option

--- SCRAME 3/src/LessonOption.java
+++ SCRAME 3/src/LessonOption.java
@@ -1,22 +1,29 @@
 
-//enum for different types of Lessons
+//enumeration for different types of Lessons
 public enum LessonOption {
     LECTURE {   //Lecture type
 
+        @Override   //return "Lecture" String
         public String toString() {
             return "Lecture";
         }
     },
     TUTORIAL {  //Tutorial type
+
+        @Override   //return "Tutorial" String
         public String toString() {
             return "Tutorial";
         }
     },
     LAB {       //Lab type
+
+        @Override   //return "Lab" String
         public String toString() {
             return "Lab";
         }
     };
+
+    //return number of lesson types
     public static int getNumLessonType() {
         return 3;
     }
