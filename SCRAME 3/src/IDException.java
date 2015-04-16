43f41e0cbd4a56ccd4e338dbe42f5da91c130d89
sngguojie <sngguojie@gmail.com>
2015-04-16 15:47:32 +0800

    i

--- SCRAME 3/src/IDException.java
+++ SCRAME 3/src/IDException.java
@@ -1,12 +1,6 @@
 
 public class IDException extends Exception {
 
-    public IDException(){
-
-        super("The ID entered is not a valid ID!");
-
-    }
-
     public IDException(String object){
 
         super("The " + object + " ID entered is not a valid ID!");
